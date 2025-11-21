a6fa9870e8a26952b6d09c3132b34f0d900aafe9
TobiTuTuebingen <tobias.tuchel@objektfabrik.de>
2025-11-21 08:40:48 +0000

    update

--- _site/gitsetup.R
+++
@@ -1,8 +0,0 @@
-library(usethis)
-
-use_git_config(user.name = "TobiTuTuebingen", 
-               user.email = "tobias.tuchel@objektfabrik.de")
-usethis::git_default_branch_configure()
-
-gitcreds::gitcreds_set()
-
